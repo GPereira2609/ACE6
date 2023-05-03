7800f8b6d5dc7777dc107f261416f85ed0ab26bc
Enrique Santos <santosenrique869@gmail.com>
2023-05-02 21:57:31 -0300

    usar conexão do banco referente ao container do docker

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker     
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root@localhost:1433/db_almoxarifado"
+SQLALCHEMY_DATABASE_URL = "postgresql://docker:root@localhost:5432/db_almoxarifado"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
