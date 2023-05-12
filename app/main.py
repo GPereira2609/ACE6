28284343e339a081d4ea9499245557159b9f2bf3
Enrique Santos <santosenrique869@gmail.com>
2023-05-12 11:10:47 -0300

    atualizar ordem do imports

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
-from app.routes.auth_routes import auth_router
-
 import uvicorn
+from fastapi import FastAPI
+
+from app.routes.auth_routes import auth_router
 
 app = FastAPI()
 app.include_router(auth_router)
