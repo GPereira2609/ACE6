5d38eaa8a9e5ef3e6e3404e4fae63ed4b199f1a7
Enrique Santos <santosenrique869@gmail.com>
2023-05-12 12:43:59 -0300

    usar url do banco do vinda do .env

--- app/database/db.py
+++ app/database/db.py
@@ -1,20 +1,25 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker     
+from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://docker:root@localhost:5432/db_almoxarifado"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.environ['DATABASE_URL']
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
-    except:
+    finally:
         db.close()
-        
