4fb4f93e3498db2ff5e70dcef1fcce77f34e35de
Enrique Santos <santosenrique869@gmail.com>
2023-05-12 12:44:34 -0300

    executar a criação das tabelas ao inciar o server

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
 
+from app.database.db import Base, engine
 from app.routes.auth_routes import auth_router
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(auth_router)
