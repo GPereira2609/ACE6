794112c8c4fbdb432534cbc88fc25e8ea5da3faa
Enrique Santos <santosenrique869@gmail.com>
2023-05-12 12:42:15 -0300

    passar a depender da session do db pelo fastApi

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,16 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
 from app.database.db import get_db
 from app.schemas.UsuarioSchema import UsuarioIn, UsuarioOut
 from app.services.auth_service import AuthService
 
-session = get_db()
-
-auth_service = AuthService(session)
-
 auth_router = APIRouter()
 
 
 @auth_router.post('/registrar', response_model=UsuarioOut)
-def register(data: UsuarioIn):
-    return auth_service.register(data)
+def register(data: UsuarioIn, session: Session = Depends(get_db)):
+    return AuthService(session).register(data)
